3189f0afb33d7fde9097e61990223153a8e01dcb
Aziz <azizsaidani1999@gmail.com>
2025-12-03 08:44:56 +0100

    feat: Integrate Firebase application, Firestore, and Storage services with environment-based configuration.

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCZ1Qe3D6_ccxpHAzjImp-u4cYJb2AX6_A",
+    authDomain: "bazz-testing-python.firebaseapp.com",
+    projectId: "bazz-testing-python",
+    storageBucket: "bazz-testing-python.firebasestorage.app",
+    messagingSenderId: "161772090075",
+    appId: "1:161772090075:web:33cbd1f928e751d87392fe",
+    measurementId: "G-2PF1F2JZ05"
+  }
+};
